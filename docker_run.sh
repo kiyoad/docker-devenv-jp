f7c0b7b8e5e014c4f0dbe798c5e0726569ca2afd
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-01-16 17:13:40 +0900

    Abolish port forwarding for ssh login
    It uses the IP of the bridge interface instead.
    
    And...
     o Add date info for the container name.
     o Use --hostname for the container.

--- docker_run.sh
+++ docker_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 
-local_ssh_port=12222
 container_name=devenv
 image_name=kiyoad/devenv
 export_local_dir=/home/kiyoad/Documents
+id=$(date '+%Y%m%d')
 
-docker run -d -p 127.0.0.1:${local_ssh_port}:22 -v ${export_local_dir}:/home/developer/Documents --name=${container_name} ${image_name}
+docker run -d -v ${export_local_dir}:/home/developer/Documents --hostname=${container_name} --name=${container_name}_${id} ${image_name}_${id}
