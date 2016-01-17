e08bfc0c4316dbadb8bd157018016edadc5df672
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-01-17 21:39:09 +0900

    Add argument number check

--- login_cntr.sh
+++ login_cntr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 CONTAINER_NAME"
+    exit 1
+fi
+
 IP=$(docker inspect $1 | jq -r '.[]|.NetworkSettings|.Networks|.bridge.IPAddress')
 ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -o 'ForwardX11 yes' developer@${IP}
