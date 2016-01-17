9dc06ea71ddf0d3038e94a5cb4a27fc555a5558d
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-01-17 12:55:52 +0900

    Remove port forwarding IP

--- mac_docker_run.sh
+++ mac_docker_run.sh
@@ -7,4 +7,4 @@ image_name=kiyoad/devenv
 export_local_dir=/Users/kiyoad/Documents
 id=$(date '+%Y%m%d')
 
-docker run -d -p 127.0.0.1:${devenv_ssh_port}:22 -v ${export_local_dir}:/home/developer/Documents --hostname=${container_name} --name=${container_name}_${id} ${image_name}_${id}
+docker run -d -p ${devenv_ssh_port}:22 -v ${export_local_dir}:/home/developer/Documents --hostname=${container_name} --name=${container_name}_${id} ${image_name}_${id}
