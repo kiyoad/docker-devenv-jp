39f413c4acb47c76c3659ee870c7754f1a3f7413
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-01-17 13:07:00 +0900

    Direct login to docker-machine's VM

--- mac_docker_portforward.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-local_ssh_port=12222
-devenv_ssh_port=12222
-devenv_ssh_host=`echo $DOCKER_HOST | sed -e 's|tcp://||' -e 's|:.*$||'`
-
-ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -N -L ${local_ssh_port}:127.0.0.1:${devenv_ssh_port} docker@${devenv_ssh_host} -i $(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r '.Driver|.SSHKeyPath')
