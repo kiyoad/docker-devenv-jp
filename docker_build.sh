ec458bec67aa37e9c010945f0978d6261646ffe3
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-07-31 17:32:14 +0900

    Fix emacs build fail

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,5 @@ set -eux
 image_name=kiyoad/devenv
 id=$(date '+%Y%m%d')
 
+sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"
 script -ac "docker build -t ${image_name}_${id} ." docker_build_${id}.log
