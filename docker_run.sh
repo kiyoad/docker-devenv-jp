239264d4ab378e115dbdd5aaa13b905c98efcfbc
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-10-09 20:52:39 +0900

    Fix export_local_dir from fixed value to ${HOME}

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,7 @@ set -eux
 
 container_name=devenv
 image_name=kiyoad/devenv
-export_local_dir=/home/kiyoad/Documents
+export_local_dir=${HOME}/Documents
 id=$(date '+%Y%m%d')
 
 docker run -d -v ${export_local_dir}:/home/developer/Documents --hostname=${container_name} --name=${container_name}_${id} ${image_name}_${id}
